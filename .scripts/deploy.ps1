a5ca9b77f86da11eecfbfe70dd562165eab11359
Michael Arranaga <marranaga@users.noreply.github.com>
2018-10-17 11:39:46 -0500

    Updated for deployment (#2)
    * Commented out GitHub deploy section
    
    * Added if statement to confirm we're tagging for production
    
    * Removed -
    
    * Added codecov secure token

--- .scripts/deploy.ps1
+++ .scripts/deploy.ps1
@@ -6,13 +6,19 @@ $PSScriptRootParent = Split-Path $PSScriptRoot -Parent
 Write-Host "[Deploy] PSScriptRootParent: ${PSScriptRootParent}" -Foregroundcolor 'Blue' -BackgroundColor 'Magenta'
 Write-Host "[Deploy] APPVEYOR_PROJECT_NAME: ${env:APPVEYOR_PROJECT_NAME}" -Foregroundcolor 'Blue' -BackgroundColor 'Magenta'
 
-Deploy Module {
-    By PSGalleryModule WallpaperManager {
-        FromSource "${PSScriptRootParent}\dev\BuildOutput\WallpaperManager"
-        To PSGallery
-        # Tagged Testing
-        WithOptions @{
-            ApiKey = $env:PSGalleryApiKey
+
+if (
+    ($env:APPVEYOR_REPO_TAG -eq 'true') -and
+    ($env:APPVEYOR_REPO_BRANCH -eq 'master')
+) {
+    Deploy Module {
+        By PSGalleryModule WallpaperManager {
+            FromSource "${PSScriptRootParent}\dev\BuildOutput\WallpaperManager"
+            To PSGallery
+            # Tagged Testing
+            WithOptions @{
+                ApiKey = $env:PSGalleryApiKey
+            }
         }
     }
 }
